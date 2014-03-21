4f8a959d60cc91ecbd892048bd4e9de526573dca
Gustavo Ferreira <gustavo@hipertipo.com>
2014-03-21 05:49:24 +0100

    adding extension builder

---
+++ build-RF-extension.py
@@ -0,0 +1,29 @@
+# [h] build hTools2 as a RoboFont Extension
+
+import hTools2
+import os
+from mojo.extensions import ExtensionBundle
+
+hTools2_path = os.path.dirname(os.path.dirname(hTools2.__file__ ))
+hTools2_html = os.path.join(os.path.dirname(hTools2_path), "Docs/build/html")
+
+extension_file = 'hTools2.roboFontExt'
+extension_path = os.path.join(os.path.dirname(__file__), extension_file)
+
+print 'building extension...',
+
+B = ExtensionBundle()
+B.name = "hTools2"
+B.developer = 'Gustavo Ferreira'
+B.developerURL = 'http://hipertipo.com/'
+B.version = "1.6"
+B.mainScript = "init-RF-extension.py"
+B.launchAtStartUp = 1
+B.addToMenu = []
+B.requiresVersionMajor = '1'
+B.requiresVersionMinor = '5'
+B.infoDictionary["repository"] = 'gferreira/hTools2'
+B.infoDictionary["html"] = 1
+B.save(extension_path, libPath=hTools2_path, htmlPath=hTools2_html, resourcesPath=None, pycOnly=False)
+
+print 'done.'
